27f146c086bd2dc61899a76ebe2457cc1061e505
mahmoudsalah239 <mamosalah239@gmail.com>
2024-05-05 17:26:42 +0300

    navbar Footer Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { NotfoundComponent } from './Components/notfound/notfound.component';
+import { SigninComponent } from './Components/signin/signin.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { DetailesComponent } from './Components/detailes/detailes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:" " ,redirectTo:"home", pathMatch:'full'},
+    {path:"home" ,component:HomeComponent},
+    {path:"details" ,component:DetailesComponent},
+    {path:"signin" ,component:SigninComponent},
+    {path:"register" ,component:RegisterComponent},
+    {path:"**" ,component:NotfoundComponent},
+];
