2e602be03a8da0bdf7887f0888246e529b755b82
3bda1137 <abdallahmahfouz111@gmail.com>
2024-05-06 11:10:10 +0300

    add Property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { FooterComponent } from './Components/footer/footer.component';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'SakanyFront';
- 
+  title = 'SakanyFront';  
 }
