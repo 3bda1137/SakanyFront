4c6a673c306c5575728dc853dc18efa18efbc87e
3bda1137 <abdallahmahfouz111@gmail.com>
2024-05-07 18:16:08 +0300

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,21 +7,13 @@ import { DetailesComponent } from './Components/detailes/detailes.component';
 import { PropertyComponent } from './Components/property/property.component';
 
 export const routes: Routes = [
-<<<<<<< HEAD
-    {path:" " ,redirectTo:"home", pathMatch:'full'},
-    {path:"home" ,component:HomeComponent},
-    {path:"details" ,component:DetailesComponent},
-    {path:"login" ,component:SigninComponent},
-    {path:"register" ,component:RegisterComponent},
-    {path:"**" ,component:NotfoundComponent},
-=======
+
     { path: " ", redirectTo: "home", pathMatch: 'full' },
     { path: "home", component: HomeComponent },
     { path: "details", component: DetailesComponent },
-    { path: "signin", component: SigninComponent },
+    { path: "login", component: SigninComponent },
     { path: "register", component: RegisterComponent },
     //test
     { path: "property", component: PropertyComponent },
     { path: "**", component: NotfoundComponent },
->>>>>>> e83a1c56466b9576919b0861a5a1d2f583a446a9
 ];
