681104ecf28859e255049f72523ff548a968ddd9
moudy99 <bobmoudy1@gmail.com>
2024-05-08 05:51:26 +0300

    add wow.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { RouterOutlet } from '@angular/router';
