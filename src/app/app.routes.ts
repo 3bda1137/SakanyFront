debc0a8ef76b25df14d35f0a236f132471c096a0
Mousa <54138093+Mousa-Ahmed98@users.noreply.github.com>
2024-05-08 19:02:41 +0300

    display and update user info

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { NotfoundComponent } from './Components/notfound/notfound.component';
 import { SigninComponent } from './Components/signin/signin.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { DetailesComponent } from './Components/detailes/detailes.component';
+import { ProfileComponent } from './Components/profile/profile.component';
 
 export const routes: Routes = [
-    {path:" " ,redirectTo:"home", pathMatch:'full'},
+    {path:"" ,redirectTo:"home", pathMatch:'full'},
     {path:"home" ,component:HomeComponent},
+    {path:"profile" ,component:ProfileComponent},
     {path:"details" ,component:DetailesComponent},
     {path:"signin" ,component:SigninComponent},
     {path:"register" ,component:RegisterComponent},
