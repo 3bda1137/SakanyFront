3df736e954fccb57041a3fae63e4592d678fa18d
moudy99 <bobmoudy1@gmail.com>
2024-05-10 11:49:52 +0300

    add search ui

---
+++ src/app/Interfaces/iapartment.ts
@@ -0,0 +1,15 @@
+export interface IApartment {
+  imageUrl: string;
+  description: string;
+  price: number;
+  id: number;
+  name: string;
+  type: string;
+  numOfBedrooms: number;
+  numOfBathrooms: number;
+  Area: number;
+  address: string;
+  ownerName: string;
+  ownerImageUrl: string;
+  postAddedTime: string;
+}
