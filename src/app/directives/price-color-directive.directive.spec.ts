c13db489a539099d55a7b1ea97a7f03f9676e49b
moudy99 <bobmoudy1@gmail.com>
2024-05-11 18:10:08 +0300

    add some custom directive

---
+++ src/app/directives/price-color-directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PriceColorDirectiveDirective } from './price-color-directive.directive';
+
+describe('PriceColorDirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PriceColorDirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
